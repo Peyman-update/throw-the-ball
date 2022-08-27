83371a250a4241af866c5a62d82e871037fd5a89
peyman <khezli1999@gmail.com>
2022-08-27 06:12:30 +0430

    Project1

--- javaScript.js
+++ javaScript.js
@@ -10,107 +10,38 @@ const c = canvas.getContext("2d");
 
 function drawCanvas() {
     return {
+        // random color
+        randomColor: function () {
+            let letters = '0123456789ABCDEF';
+            let color = '#';
 
-        Rectangle: function () {
+            for (let index = 0; index < 6; index++) {
 
-            c.fillStyle = "red";
-            c.fillRect(100, 100, 100, 50);
-            c.fillStyle = "green";
-            c.fillRect(300, 200, 100, 50);
-            c.fillStyle = "red";
-            c.fillRect(500, 100, 100, 50);
-            c.fill();
+                color += letters[Math.floor(Math.random() * 16)]
+            }
+
+            return color;
         },
 
-        Line: function () {
+        projet1: function () {
 
-            c.beginPath();
-            c.lineTo(200, 300)
-            c.lineTo(400, 500)
-            c.lineTo(600, 400)
-            c.lineTo(700, 400)
-            c.lineTo(700, 600)
-            c.strokeStyle = 'red'
-            c.stroke()
-        },
-    }
-}
+            setInterval(() => {
 
+                let x = Math.random() * window.innerWidth;
+                let y = Math.random() * window.innerHeight;
+                c.beginPath();
+                c.arc(x, y, 50, 0, 2 * Math.PI);
+                c.strokeStyle = this.randomColor();
+                c.stroke();
 
-// constructor function
+            }, 600)
 
-function DrawCanvas() {
-
-    this.getRandomColor = function () {
-
-        let letters = '0123456789ABCDEF'
-
-        let color = '#';
-
-        for (let r = 0; r < 6; r++) {
-
-            color += letters[Math.floor(Math.random() * 16)]
 
         }
 
-        return color;
-
-    }
-
-
-    this.Rectangle = () => {
-        c.strokeStyle = this.getRandomColor()
-        c.strokeRect(100, 100, 200, 100);
-        c.strokeStyle = this.getRandomColor()
-        c.strokeRect(300, 300, 200, 100);
-        c.strokeStyle = this.getRandomColor()
-        c.strokeRect(500, 100, 200, 100);
-        c.strokeStyle = this.getRandomColor()
-        c.strokeRect(700, 300, 200, 100);
-        c.stroke();
-
-    }
-    this.Line = () => {
-
-        c.beginPath();
-        c.lineTo(100, 300)
-        c.lineTo(500, 600)
-        c.lineTo(400, 200)
-        c.lineTo(100, 200)
-        c.lineTo(700, 150)
-        c.strokeStyle = this.getRandomColor()
-        // Connect the beginning of the line to the end of the line
-        c.closePath()
-        // drawing shape
-        c.stroke()
-
-    }
-
-    this.Circle = () => {
-
-        let i = 0;
-
-        do {
-
-            let x = Math.random() * window.innerWidth;
-            let y = Math.random() * window.innerHeight;
-            c.beginPath()
-            c.arc(x, y, 50, 0, 2 * Math.PI);
-            c.strokeStyle = this.getRandomColor();
-            c.stroke();
-
-            i++;
-
-        } while (i <= 100)
-
     }
 }
 
+let re = drawCanvas();
 
-const result = new DrawCanvas();
-
-//result.Rectangle();
-//result.Line();
-result.Circle();
-
-//const result = drawCanvas().Line();
+re.projet1();
