1d95d88959c39a87eb96d642635923c452def81e
peyman <khezli1999@gmail.com>
2022-08-28 18:10:44 +0430

    Completion of the previous project

--- javaScript.js
+++ javaScript.js
@@ -9,11 +9,14 @@ let c = canvas.getContext('2d');
 
 class Ball {
 
-    constructor() {
+    constructor(x, y) {
 
-        this.r = Math.floor(Math.random() * 40);
-        this.x = randomWidth(0 - this.r, window.innerWidth + this.r);
-        this.y = randomWidth(0 - this.r, window.innerHeight + this.r);;
+        // Properties of ball
+
+        this.baser = 10;
+        this.r = this.baser;
+        this.x = x || randomWidth(0 - this.r, window.innerWidth + this.r);
+        this.y = y || randomWidth(0 - this.r, window.innerHeight + this.r);;
         this.xv = (Math.random() - 0.5) * 30;
         this.yv = (Math.random() - 0.5) * 30;
         this.draw();
@@ -21,6 +24,7 @@ class Ball {
 
     }
 
+    // drawing  ball
     draw() {
 
         c.beginPath();
@@ -28,12 +32,14 @@ class Ball {
         c.fill()
     }
 
+    //Game logic
+
     update() {
 
         if (this.x + this.r > window.innerWidth || this.x - this.r < 0) {
             this.xv = -this.xv;
             c.fillStyle = this.color;
-        
+
         }
 
         if (this.y + this.r > window.innerHeight || this.y - this.r < 0) {
@@ -44,32 +50,52 @@ class Ball {
         this.x += this.xv;
         this.y += this.yv;
         this.draw();
-}
+    }
 }
 
 
 let ball = [];
 
-for (let index = 0; index < 200; index++) {
-
-    ball.push(new Ball)
-
-}
+// animation 
 
 function animate() {
-
     c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
     ball.forEach(ball => {
         ball.update();
     })
-
     requestAnimationFrame(animate);
 }
 
+// Production new ball 
+
+window.addEventListener('click', (e) => {
+    ball.push(new Ball(e.clientX, e.clientY))
+})
+
+
+// big ball when mousemove
+
+window.addEventListener('mousemove', function (e) {
+    ball.forEach(ball => {
+        let distance = Math.sqrt(Math.pow(e.clientX - ball.x, 2) + Math.pow(e.clientY - ball.y, 2));
+        console.log(distance);
+        if (distance < 50 && ball.r < ball.baser * 4) {
+            ball.r += 1
+        } else if (ball.r > ball.baser && distance > 50) {
+            ball.r -= 1
+        }
+    })
+
+})
+
+// Responsive canvas
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+})
 
 animate();
-
 
 function Color() {
 
@@ -81,7 +107,6 @@ function Color() {
 
     return color;
 }
-
 
 function randomWidth(min, max) {
 
